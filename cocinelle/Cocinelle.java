f3c17597e5f3045d02b30685b8ce07e6686cde84
lebonq <57987762+lebonq@users.noreply.github.com>
2020-05-07 15:04:36 +0200

    Initial commit

---
+++ cocinelle/Cocinelle.java
@@ -0,0 +1,5 @@
+public class Cocinelle{
+    static public void main(final String[] args){
+        System.out.println("Hello ladybug");
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,6 @@
+
+public class Main{
+    static public void main(final String[] args){
+        System.out.println("Hello World");
+    }
+}
