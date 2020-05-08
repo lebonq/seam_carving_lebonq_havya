3d98d63be8869860eba467ced06deef37e5ad644
lebonq <57987762+lebonq@users.noreply.github.com>
2020-05-08 17:37:16 +0200

    Version finale Coccinelle 3
    Co-authored-by: audrik@live.fr <audrik@live.fr>

--- coccinelle/Coccinelle.java
+++ coccinelle/Coccinelle.java
@@ -74,14 +74,14 @@ public class Coccinelle{
         int vLMax = pM.length;
         int vIndiceMax = afficherValeurMax(pM,vCMax);
         System.out.print("Elle a suivi le chemin : ");
-        int[][] vDebutFin = accm(pM,vLMax-1,vIndiceMax,0);
-        System.out.printf("\nCase d'atterrissage = (%d,%d) \n",vDebutFin[0][0],vDebutFin[0][1]);
-        System.out.printf("Case de l'interview = (%d,%d)",vDebutFin[01][0],vDebutFin[1][1]);
+        int[] vDebutFin = accm(pM,vLMax-1,vIndiceMax);
+        System.out.printf("\nCase d'atterrissage = (%d,%d) \n",vDebutFin[0],vDebutFin[1]);
+        System.out.printf("Case de l'interview = (%d,%d)",pM.length-1,vIndiceMax);
 
     }//afficherResultat()
 
     static int afficherValeurMax(final int[][] pM,final int pCMax){
-        int vRes = pM[7][0];
+        int vRes = pM[pM.length-1][0];
         int vIndice = 0;
         for(int i=0; i<pCMax-1;i++){    
             if(vRes < pM[7][i + 1]){
@@ -97,29 +97,33 @@ public class Coccinelle{
      * Retourne un tableau 2d ou a l'indice 0 : tableau contenant les coordonees de la case d'interview
      *                             l'indice 1 : tableau contenant les coordonees de la case d'atterrissage
      */
-    static int[][] accm(final int[][] pM,final int pI,final int pJ,final int pJFinal){ 
-        int vMoinsUn = 0;
-        int[][] vReturn = new int[2][2];
-        if(pI == -1)return new int[][]{{pI+1,pJ},{pM.length-1,pJFinal}};
+    static int[] accm(final int[][] pM,final int pI,final int pJ){ 
+        int vJMoinsUn = 0;
+        int vJPlusUn = 0;
+        int[] vReturn = new int[2];
+        if(pI == -1)return new int[]{pI+1,pJ};
         try{
-            vMoinsUn = pM[pI][pJ-1];
+            vJMoinsUn = pM[pI][pJ-1];
         }//endtry
         catch(Exception pE){};
-        if(pM[pI][pJ+1] < vMoinsUn && vMoinsUn > pM[pI][pJ]){
-            if(pI == pM.length) accm(pM,pI-1, pJ+1,pJ);
-            vReturn = accm(pM,pI-1,pJ-1,pJFinal);
+        try{
+            vJPlusUn = pM[pI][pJ+1];
+        }//endtry
+        catch(Exception pE){};
+
+        if(vJPlusUn < vJMoinsUn && vJMoinsUn > pM[pI][pJ]){
+            vReturn = accm(pM,pI-1,pJ-1);
             System.out.printf("(%d,%d)",pI,pJ-1);
         }//endif
-        else if (pM[pI][pJ+1] < pM[pI][pJ]){
-            if(pI == pM.length) accm(pM,pI-1, pJ+1,pJ);
-            vReturn = accm(pM,pI-1,pJ,pJFinal);
+        else if (vJPlusUn < pM[pI][pJ]){
+            vReturn = accm(pM,pI-1,pJ);
             System.out.printf("(%d,%d)",pI,pJ);
         }//end elseif
         else {
-            if(pI == pM.length) accm(pM,pI-1, pJ+1,pJ);
-            vReturn = accm(pM,pI-1, pJ+1,pJFinal);
+            vReturn = accm(pM,pI-1, pJ+1);
             System.out.printf("(%d,%d)",pI,pJ+1);
         }//end else
+
         return vReturn;
     }//accm()
 
