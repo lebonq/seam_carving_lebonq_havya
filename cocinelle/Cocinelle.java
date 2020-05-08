381afbc0588f1077477fd1124c74db98bab34b58
lebonq <57987762+lebonq@users.noreply.github.com>
2020-05-08 10:38:32 +0200

    Ajout de commentaire

--- cocinelle/Cocinelle.java
+++ cocinelle/Cocinelle.java
@@ -16,30 +16,31 @@ public class Cocinelle{
             // coût min si le dernier déplacement est Nord
             int Mne = M[l-1][c-1] + ne(l-1,c-1); // coût min si le dernier déplacement est Nord—Est
             M[l][c] = (int)Math.min(Me, (int)Math.min(Mn,Mne));
-        }
+        }//endfor
     return M;
-}
+    }// int[][]calculerM()
 
-static void afficherCheminCoutMin( int[][]M) { accm(M,L,C);}
-    
-static void accm(int[][] M, int l, int c){
-    if(l==0 && c==0) return;
-    int Me = I, Mn = I, Mne = I;
-    if ((c-1>=0)) Me=M[l][c-1] + e(l,c-1);
-    if ((l-1>=0)) Me=M[l-1][c] + n(l-1,c);
-    if ((l-1>=0 && c-1>=0)) Mne=M[l-1][c-1] + ne(l-1,c-1);
-    if(M[l][c] == Me){ 
-        accm(M,l,c-1);
-        System.out.printf("(%d,%d)-->",l,c-1);
-    }
-    else
-        if (M[l][c] == Mn){ 
-            accm(M, l-1, c);
-            System.out.printf("(%d,%d) -->", l-1,c);
-        }
-        else {
-            accm(M, l-1, c-1);
-            System.out.printf("(&d,%d)", l-1,c-1);
+    static void afficherCheminCoutMin( int[][]M) { accm(M,L,C);}
+        
+    static void accm(int[][] M, int l, int c){
+        if(l==0 && c==0) return;
+        int Me = I, Mn = I, Mne = I;
+        if ((c-1>=0)) Me=M[l][c-1] + e(l,c-1);
+        if ((l-1>=0)) Me=M[l-1][c] + n(l-1,c);
+        if ((l-1>=0 && c-1>=0)) Mne=M[l-1][c-1] + ne(l-1,c-1);
+        if(M[l][c] == Me){ 
+            accm(M,l,c-1);
+            System.out.printf("(%d,%d)-->",l,c-1);
         }//endif
+        else
+            if (M[l][c] == Mn){ 
+                accm(M, l-1, c);
+                System.out.printf("(%d,%d) -->", l-1,c);
+            }//endif
+            else {
+                accm(M, l-1, c-1);
+                System.out.printf("(&d,%d)", l-1,c-1);
+            }//endelse
     }//Methode accm(M,l,c)
 }//Class cocinelle
+//Je vais me prendre un cafe
