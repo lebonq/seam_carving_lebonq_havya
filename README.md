e9ed565888465e23f04f82f931eb5fe6d83d6e70
lebonq <57987762+lebonq@users.noreply.github.com>
2020-05-11 13:58:44 +0200

    Debut de la redimension

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Projet_algo
 Pour un 20/20 assure.
+# Algo pour seam carving
+https://stackoverflow.com/questions/18809192/what-does-image-gradient-mean-in-the-following-paper
+https://en.wikipedia.org/wiki/Edge_detection
--- src/SeamCarving.java
+++ src/SeamCarving.java
@@ -1,5 +1,47 @@
+import java.awt.image.BufferedImage;
+import static java.awt.image.BufferedImage.TYPE_INT_RGB;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
 public class SeamCarving{
+
+    static public int[][] createImageTab(final String pImageName) throws Exception{
+
+        BufferedImage vImage = ImageIO.read(new File(pImageName));
+
+        int vImageWidth = vImage.getWidth();
+        int vImageHeight = vImage.getHeight();
+        int[][] vImageTab = new int[vImageHeight][vImageWidth];
+
+        for(int i = 0; i < vImageHeight;i++){
+            for(int j = 0; j < vImageWidth;j++){
+                vImageTab[i][j]= vImage.getRGB(i, j);
+            }
+        }
+        System.out.print(vImageTab);
+        return vImageTab;
+    }
+
+    static public void createFile(final int[][] pImageTab) throws Exception{
+        File vOutputfile = new File("image.jpg");
+        BufferedImage vImage = new BufferedImage(256,256,TYPE_INT_RGB);
+
+        for(int i = 0; i < 512;i++){
+            for(int j = 0; j < 512;j++){
+                vImage.setRGB(i/2, j/2, pImageTab[i][j]);
+            }
+        }
+
+        ImageIO.write(vImage, "jpg", vOutputfile);
+    }
+
     static public void main(final String[] args){
-        System.out.println("Hello World");
+        try{
+            createFile(createImageTab("src/chaton.jpg"));
+        }
+        catch(Exception pE){
+            System.err.println(pE);
+        }
     }
 }
